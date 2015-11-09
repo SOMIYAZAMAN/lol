16a6623b3270eaf1f1853ca77315ba3da40b6c4b
FA12-BSE-221 <FA12-BSE-221@LAB7C-13.cns.ciit>
2015-11-09 11:09:41 +0500

    play2

--- Demo/src/Git.java
+++ Demo/src/Git.java
@@ -4,7 +4,8 @@ public class Git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("lets play with GIT");
-System.out.println("lol");
+System.out.println("2ndplay");
+System.out.println("3rdplay");
 	}
 
 }
