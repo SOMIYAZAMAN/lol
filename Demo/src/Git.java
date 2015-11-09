282fe263d131f8fe8de326e37e2e1118603ae250
FA12-BSE-221 <FA12-BSE-221@LAB7C-13.cns.ciit>
2015-11-09 10:09:01 +0500

    i have made changes

---
+++ Demo/src/Git.java
@@ -0,0 +1,10 @@
+
+public class Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("lets play with GIT");
+System.out.println("lol");
+	}
+
+}
